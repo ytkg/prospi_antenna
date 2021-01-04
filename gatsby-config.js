264e324a3292fa9d789fcd3479bf84e99822eefa
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-01-05 08:41:00 +0900

    [update] サイト情報

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `プロスピアンテナ`,
+    description: `プロスピA関連の記事やYouTube動画を紹介します。`,
+    siteUrl: `https://prospi-antenna.vercel.app/`,
   },
   plugins: [
     {
