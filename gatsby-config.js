01c94d542d1861a40da8c857fbfff7a9e6611440
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-01-05 08:50:59 +0900

    [update] GA trackingID

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-WZ2XR5C06M`,
       },
     },
     `gatsby-plugin-feed`,
